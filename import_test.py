e0157a0735eed3b6af9c682960df67208b119159
Eddie Antonio Santos <eddieantonio@hey.com>
2021-02-03 09:56:20 -0700

    fix: missing types on some tests

--- import_test.py
+++ import_test.py
@@ -10,25 +10,25 @@ TEST_FST = "crk-relaxed-analyzer-for-dictionary.hfstol"
 
 # scope="session" reuses the FST for all tests that use this fixture
 @pytest.fixture(scope="session")
-def fst():
+def fst() -> TransducerFile:
     return TransducerFile(TEST_FST)
 
 
-def test_has_version():
+def test_has_version() -> None:
     assert type(hfst_optimized_lookup.__version__) == str
 
 
-def test_symbol_count():
+def test_symbol_count() -> None:
     # If this returned a non-number, we’d get a TypeError here.
     assert TransducerFile(TEST_FST).symbol_count() > 0
 
 
-def test_subsequent_lookups(fst: TransducerFile):
+def test_subsequent_lookups(fst: TransducerFile) -> None:
     assert fst.lookup("itwêwina") == ["itwêwin+N+I+Pl"]
     assert fst.lookup("nikî-nipân") == ["PV/ki+nipâw+V+AI+Ind+1Sg"]
 
 
-def test_multiple_analyses(fst: TransducerFile):
+def test_multiple_analyses(fst: TransducerFile) -> None:
     assert fst.lookup("môswa") == ["môswa+N+A+Sg", "môswa+N+A+Obv"]
 
 
@@ -39,28 +39,28 @@ EXPECTED_BULK_LOOKUP_RESULT_1 = {
 }
 
 
-def test_bulk_lookup(fst: TransducerFile):
+def test_bulk_lookup(fst: TransducerFile) -> None:
     assert (
         fst.bulk_lookup(EXPECTED_BULK_LOOKUP_RESULT_1.keys())
         == EXPECTED_BULK_LOOKUP_RESULT_1
     )
 
 
-def test_bulk_lookup_with_iterator(fst: TransducerFile):
+def test_bulk_lookup_with_iterator(fst: TransducerFile) -> None:
     assert (
         fst.bulk_lookup(w for w in EXPECTED_BULK_LOOKUP_RESULT_1.keys())
         == EXPECTED_BULK_LOOKUP_RESULT_1
     )
 
 
-def test_create_from_path_obj():
+def test_create_from_path_obj() -> None:
     fst = TransducerFile(Path(TEST_FST))
     assert fst.lookup("itwêwina") == ["itwêwin+N+I+Pl"]
 
 
 @pytest.mark.skip("not yet implemented")
-def test_limit(fst):
-    assert fst.lookup("môswa", limit=1) == ["môswa+N+A+Sg"]
+def test_limit(fst: TransducerFile) -> None:
+    assert fst.lookup("môswa", limit=1) == ["môswa+N+A+Sg"]  # type: ignore
 
 
 @pytest.mark.parametrize(
@@ -73,14 +73,17 @@ def test_limit(fst):
                 ["m", "ô", "s", "w", "a", "+N", "+A", "+Obv"],
             ],
         ],
-        ["nikî-nipân", [["PV/ki+", "n", "i", "p", "â", "w", "+V", "+AI", "+Ind", "+1Sg"]]],
+        [
+            "nikî-nipân",
+            [["PV/ki+", "n", "i", "p", "â", "w", "+V", "+AI", "+Ind", "+1Sg"]],
+        ],
     ],
 )
-def test_symbol_lookup1(fst, surface, deep):  #: TransducerFile
+def test_symbol_lookup1(fst: TransducerFile, surface: str, deep) -> None:
     assert fst.lookup_symbols(surface) == deep
 
 
-def test_raises_exception_on_missing_file():
+def test_raises_exception_on_missing_file() -> None:
     with pytest.raises(Exception) as exception_info:
         TransducerFile("/does-not-exist.hfstol")
 
@@ -89,7 +92,7 @@ def test_raises_exception_on_missing_file():
     assert "‘/does-not-exist.hfstol’" in exception_messages
 
 
-def test_raises_exception_on_invalid_file():
+def test_raises_exception_on_invalid_file() -> None:
     with pytest.raises(Exception) as exception_info:
         # The python test file is not a valid transducer.
         TransducerFile(__file__)
